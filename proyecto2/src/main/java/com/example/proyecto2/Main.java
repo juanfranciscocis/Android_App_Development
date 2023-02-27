4942c56d0d119972702c1a6f6cec3a2ba94a37a9
juancisneros <juanfrancistm2011@icloud.com>
2023-02-26 23:43:39 -0500

    Finalizado listo para entregar

--- proyecto2/src/main/java/com/example/proyecto2/Main.java
+++ proyecto2/src/main/java/com/example/proyecto2/Main.java
@@ -11,6 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("mainPage.fxml"));
+        mainPageController controller = new mainPageController();
+        fxmlLoader.setController(controller);
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Calc Factorial");
         stage.setScene(scene);
